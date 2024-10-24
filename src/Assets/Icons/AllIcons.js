33c9a66710fb38b11dcb5633abd0afaeb8b12cf7
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-10-24 22:17:46 +0300

    New Update

---
+++ src/Assets/Icons/AllIcons.js
@@ -0,0 +1,6 @@
+export { default as HomeIcon } from "./HomeIcon";
+export { default as PosIcon } from "./PosIcon";
+export { default as OrderIcon } from "./OrderIcon";
+export { default as CategoryIcon } from "./CategoryIcon";
+
+export { default as WarningIcon } from "./WarningIcon";
