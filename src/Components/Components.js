95b7e40ff731c074c1a062eafb3c154fe6864542
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-10-24 22:17:51 +0300

    New Update

---
+++ src/Components/Components.js
@@ -0,0 +1,9 @@
+/* Buttons */
+export { default as StaticButton } from "./Buttons/StaticButton";
+export { default as AddButton } from "./Buttons/AddButton";
+/* Inputs */
+export { default as TextInput } from "./Inputs/TextInput";
+export { default as PasswordInput } from "./Inputs/PasswordInput";
+export { default as DateInput } from "./Inputs/DateInput";
+/* Loaders */
+export { default as Loading } from "./Loaders/Loading";
