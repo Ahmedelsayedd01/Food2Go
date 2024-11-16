888446503b1455ff349d886f3036b6fb1aa0a1a3
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-11-16 09:45:58 +0200

    manage export layouts

--- src/layouts/Layouts.js
+++ src/layouts/Layouts.js
@@ -4,7 +4,21 @@ export { default as LoginLayout } from "./Authentication/LoginLayout";
 // Forget Password
 export { default as ForgetPassLayout } from "./Authentication/ForgetPassLayout";
 
-
 /* Dashboard */
 // Home
 export { default as DashboardLayout } from "./Dashboard/Home/DashboardLayout";
+
+/* Addons */
+export { default as AddonsLayout } from "./Dashboard/Addons/AddonsLayout";
+export { default as EditAddonsLayout } from "./Dashboard/Addons/EditAddonsLayout";
+
+/* Category Setup */
+// Category Setup -> Category
+export { default as CategoryLayout } from "./Dashboard/CategorySetup/CategoryLayout";
+export { default as EditCategoryLayout } from "./Dashboard/CategorySetup/EditCategoryLayout";
+
+/*  Product Setup */
+// Product Setup -> Product
+export { default as ProductLayout } from "./Dashboard/ProductSetup/ProductLayout";
+export { default as EditProductLayout } from "./Dashboard/ProductSetup/EditProductLayout";
+export { default as AddProductLayout } from "./Dashboard/ProductSetup/AddProductLayout";
