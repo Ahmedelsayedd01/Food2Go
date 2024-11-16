c963d4e32c3541e6c0e838bd098d0f32aff08911
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-11-16 09:44:44 +0200

    manage export icons && components && Pages

--- src/Assets/Icons/AllIcons.js
+++ src/Assets/Icons/AllIcons.js
@@ -1,6 +1,9 @@
-export { default as HomeIcon } from "./HomeIcon";
-export { default as PosIcon } from "./PosIcon";
-export { default as OrderIcon } from "./OrderIcon";
-export { default as CategoryIcon } from "./CategoryIcon";
+export { default as HomeIcon } from "./SidebarIcons/HomeIcon";
+export { default as PosIcon } from "./SidebarIcons/PosIcon";
+export { default as OrderIcon } from "./SidebarIcons/OrderIcon";
+export { default as CategoryIcon } from "./SidebarIcons/CategoryIcon";
+export { default as ProductIcon } from "./SidebarIcons/ProductIcon";
 
-export { default as WarningIcon } from "./WarningIcon";
+export { default as EditIcon } from "./AnotherIcons/EditIcon";
+export { default as DeleteIcon } from "./AnotherIcons/DeleteIcon";
+export { default as WarningIcon } from "./AnotherIcons/WarningIcon";
--- src/Pages/Pages.js
+++ src/Pages/Pages.js
@@ -3,6 +3,19 @@
 export { default as LoginPage } from "./Authentication/LoginPage";
 // Forget Password
 export { default as ForgetPassPage } from "./Authentication/ForgetPassPage";
+
 /* Dashboard */
 // Home
-export { default as DashboardPage } from "./Dashboard/Home/DashboardPage";
+export { default as HomePage } from "./Dashboard/Admin/Home/HomePage";
+// Addons
+export { default as AddonsPage } from "./Dashboard/Admin/Addons/AddonsPage";
+export { default as AddAddonsSection } from "./Dashboard/Admin/Addons/AddAddonsSection";
+export { default as EditAddonsPage } from "./Dashboard/Admin/Addons/EditAddonsPage";
+// Category Setup
+export { default as CategoryPage } from "./Dashboard/Admin/CategorySetup/CategoryPage";
+export { default as AddCategorySection } from "./Dashboard/Admin/CategorySetup/AddCategorySection";
+export { default as EditCategoryPage } from "./Dashboard/Admin/CategorySetup/EditCategoryPage";
+// Product Setup
+export { default as ProductPage } from "./Dashboard/Admin/ProductSetup/ProductPage";
+export { default as AddProductPage } from "./Dashboard/Admin/ProductSetup/AddProductPage";
+export { default as EditProductPage } from "./Dashboard/Admin/ProductSetup/EditProductPage";
