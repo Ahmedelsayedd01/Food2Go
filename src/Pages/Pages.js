f61617709a63492dddc114f0572bac2dd3ab3a93
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-12-03 15:41:19 +0200

    new update

--- src/Pages/Pages.js
+++ src/Pages/Pages.js
@@ -23,7 +23,27 @@ export { default as EditProductPage } from "./Dashboard/Admin/ProductSetup/EditP
 export { default as PaymentMethodPage } from "./Dashboard/Admin/Setting/PaymentMethod/PaymentMethodPage";
 export { default as AddPaymentMethodSection } from "./Dashboard/Admin/Setting/PaymentMethod/AddPaymentMethodSection";
 export { default as EditPaymentMethodPage } from "./Dashboard/Admin/Setting/PaymentMethod/EditPaymentMethodPage";
+// Orders Payment
+export { default as OrdersPaymentPendingPage } from "./Dashboard/Admin/OrdersPayment/OrdersPaymentPendingPage";
+export { default as OrdersPaymentHistoryPage } from "./Dashboard/Admin/OrdersPayment/OrdersPaymentHistoryPage";
+// Addons
+export { default as AddDeliveryManSection } from "./Dashboard/Admin/DeliveryMan/AddDeliveryManSection";
+export { default as EditDeliveryManPage } from "./Dashboard/Admin/DeliveryMan/EditDeliveryManPage";
+export { default as DeliveryManPage } from "./Dashboard/Admin/DeliveryMan/DeliveryManPage";
+
 // Orders
-export { default as AllOrdersPage } from "./Dashboard/Admin/Orders/AllOrders/AllOrdersPage";
 export { default as DetailsOrdersPage } from "./Dashboard/Admin/Orders/DetailsOrder/DetailsOrderPage";
 export { default as InvoiceOrdersPage } from "./Dashboard/Admin/Orders/InvoiceOrder/InvoiceOrderPage";
+export { default as SelectDateRangeSection } from "./Dashboard/Admin/Orders/SelectDateRangeSection";
+
+export { default as CartsOrderSection } from "./Dashboard/Admin/Orders/AllOrders/CartsOrderSection";
+export { default as AllOrdersPage } from "./Dashboard/Admin/Orders/AllOrders/AllOrdersPage";
+export { default as PendingOrdersPage } from "./Dashboard/Admin/Orders/PendingOrders/PendingOrdersPage";
+export { default as ConfirmedOrdersPage } from "./Dashboard/Admin/Orders/ConfirmedOrders/ConfirmedOrdersPage";
+export { default as ProcessingOrdersPage } from "./Dashboard/Admin/Orders/ProcessingOrders/ProcessingOrdersPage";
+export { default as OutForDeliveryOrdersPage } from "./Dashboard/Admin/Orders/OutForDeliveryOrders/OutForDeliveryOrdersPage";
+export { default as DeliveredOrdersPage } from "./Dashboard/Admin/Orders/DeliveredOrders/DeliveredOrdersPage";
+export { default as ReturnedOrdersPage } from "./Dashboard/Admin/Orders/ReturnedOrders/ReturnedOrdersPage";
+export { default as FailedOrdersPage } from "./Dashboard/Admin/Orders/FailedOrders/FailedOrdersPage";
+export { default as CanceledOrdersPage } from "./Dashboard/Admin/Orders/CanceledOrders/CanceledOrdersPage";
+export { default as ScheduleOrdersPage } from "./Dashboard/Admin/Orders/ScheduleOrders/ScheduleOrdersPage";
