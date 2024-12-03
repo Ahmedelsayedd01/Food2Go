7f58b574cbe599232aa53707f3616fe7298fd6bf
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-12-03 15:22:48 +0200

    add orders icon

--- src/Assets/Icons/AllIcons.js
+++ src/Assets/Icons/AllIcons.js
@@ -7,3 +7,12 @@ export { default as ProductIcon } from "./SidebarIcons/ProductIcon";
 export { default as EditIcon } from "./AnotherIcons/EditIcon";
 export { default as DeleteIcon } from "./AnotherIcons/DeleteIcon";
 export { default as WarningIcon } from "./AnotherIcons/WarningIcon";
+
+export { default as PendingIcon } from "./AnotherIcons/PendingIcon";
+export { default as ConfirmedIcon } from "./AnotherIcons/ConfirmedIcon";
+export { default as ProcessingIcon } from "./AnotherIcons/ProcessingIcon";
+export { default as OutForDeliveryIcon } from "./AnotherIcons/OutForDeliveryIcon";
+export { default as DeliveredIcon } from "./AnotherIcons/DeliveredIcon";
+export { default as CanceledIcon } from "./AnotherIcons/CanceledIcon";
+export { default as ReturnedIcon } from "./AnotherIcons/ReturnedIcon";
+export { default as FailedToDeliverIcon } from "./AnotherIcons/FailedToDeliverIcon";
