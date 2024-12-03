44e160105bf1cfde46cda88313d5b72288146144
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-12-03 15:46:15 +0200

    new update

--- src/ProtectedData/ProtectedLogin.jsx
+++ src/ProtectedData/ProtectedLogin.jsx
@@ -9,14 +9,14 @@ const ProtectedLogin = () => {
        const location = useLocation();
        const user = useSelector(state => state.user)
        useEffect(() => {
-              const isAuthRoute = location.pathname === '/login' || location.pathname === '/forget_password';
+              const isAuthRoute = location.pathname === '/' || location.pathname === '/forget_password';
 
               if (user && isAuthRoute) {
-                     // If user is logged in and trying to access `/login` or `/forget_password`, redirect to dashboard
+                     // If user is logged in and trying to access `/` or `/forget_password`, redirect to dashboard
                      navigate('/dashboard', { replace: true });
               } else if (!user && !isAuthRoute) {
                      // If user is not logged in and trying to access a protected route, redirect to login
-                     navigate('/login', { state: { from: location }, replace: true });
+                     navigate('/', { state: { from: location }, replace: true });
               }
               // Otherwise, allow the requested route to render
        }, [user, location.pathname]);
