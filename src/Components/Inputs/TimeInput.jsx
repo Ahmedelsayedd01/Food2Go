7ea58332958a06444661a572b782359fa475e866
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-12-04 16:56:48 +0200

    new UPdate

--- src/Components/Inputs/TimeInput.jsx
+++ src/Components/Inputs/TimeInput.jsx
@@ -1,15 +1,13 @@
 import classNames from 'classnames';
 
 const TimeInput = ({ required = true, borderColor = "none", value, onChange }) => {
-       // const currentDay = new Date(); // Define currentDay
-       // const formattedDate = currentDay.toISOString().split('T')[0]; // Format as YYYY-MM-DD
 
        return (
               <>
                      <input
                             type="time"
                             className={classNames(
-                                   'w-full border-2 rounded-2xl outline-none px-2 py-3 text-2xl text-thirdColor',
+                                   'w-full border-2 rounded-2xl outline-none px-2 py-3 shadow text-2xl text-thirdColor',
                                    {
                                           'border-none': borderColor === 'none',
                                           'border-mainColor': borderColor === 'mainColor',
