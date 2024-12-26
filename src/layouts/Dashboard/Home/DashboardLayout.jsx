81fb8992a22bfaf30ae794240ea21598efe4c427
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-12-26 15:17:30 +0200

    add title to page

--- src/layouts/Dashboard/Home/DashboardLayout.jsx
+++ src/layouts/Dashboard/Home/DashboardLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { HomePage } from '../../../Pages/Pages'
+import { TitlePage } from '../../../Components/Components'
 
 const DashboardLayout = () => {
        return (
               <>
+                     <TitlePage text={'Dashboard'} />
                      <HomePage />
               </>
        )
