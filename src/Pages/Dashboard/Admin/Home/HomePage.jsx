d7019d3549d082dcac37212544561de551a5299e
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-12-26 15:19:15 +0200

    remove old chart

--- src/Pages/Dashboard/Admin/Home/HomePage.jsx
+++ src/Pages/Dashboard/Admin/Home/HomePage.jsx
@@ -4,33 +4,11 @@ import CartsOrderSection from '../Orders/AllOrders/CartsOrderSection'
 import { LoaderLogin } from '../../../../Components/Components'
 import { OrdersComponent } from '../../../../Store/CreateSlices'
 import { useGet } from '../../../../Hooks/useGet'
-// import LineChartComponent from '../../../../Components/Charts/LineChart'
-// import LineChartComponent from '../../../../Components/Charts/LineChart'
-import LineChartComponent from '../../../../Components/Charts/LineChart'
-// import { pData,uData,xLabels } from '../../../../Components/Charts/data'
 
 const HomePage = () => {
        const { refetch: refetchCountOrders, loading, data: dataCountOrders } = useGet({
               url: "https://Bcknd.food2go.online/admin/order/count",
        });
-       const mockData = {
-              pData: [
-                { x: 'Jan', y: 100 },
-                { x: 'Feb', y: 150 },
-                { x: 'Mar', y: 200 },
-                { x: 'Apr', y: 250 },
-                { x: 'May', y: 300 },
-              ],
-              uData: [
-                { x: 'Jan', y: 80 },
-                { x: 'Feb', y: 120 },
-                { x: 'Mar', y: 180 },
-                { x: 'Apr', y: 220 },
-                { x: 'May', y: 280 },
-              ],
-              xLabels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
-            };
-            
 
        useEffect(() => {
               console.log("Fetching Count Orders...");
@@ -38,7 +16,7 @@ const HomePage = () => {
        }, [refetchCountOrders]);
 
        const userName = useSelector(state => state.user.name)
-       
+
        const counters = {
               ordersAll: dataCountOrders?.orders || 0,
               ordersPending: dataCountOrders?.pending || 0,
@@ -55,7 +33,6 @@ const HomePage = () => {
        return (
               <>
                      <OrdersComponent />
-                     <strong>{userName || 'asada'}</strong>
                      <div className="w-full flex flex-col mb-0">
                             {loading ? (
                                    <>
@@ -66,11 +43,6 @@ const HomePage = () => {
                             ) : (
                                    <>
                                           <CartsOrderSection ordersNum={counters} />
-
-                                          <LineChartComponent pData={mockData.pData} uData={mockData.uData} xLabels={mockData.xLabels}/>
-                                          {/* <div className="">heloo</div> */}
-
-                                        
                                    </>
                             )}
                      </div>
